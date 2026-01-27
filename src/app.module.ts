1d86297b5f3b05a57c400bf6dee97d2917c1de5f
yaoleksa <mryaremchyk@gmail.com>
2026-01-27 10:20:17 +0200

    final commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,15 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MeController } from './me.controller';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: process.env.NODE_ENV ? `src/config/.env.${process.env.NODE_ENV}` : 'src/config/.env'
-  })],
-  controllers: [AppController, MeController, UsersController],
-  providers: [AppService, UsersService],
+  }), UsersModule],
+  controllers: [AppController, MeController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {}
