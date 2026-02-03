a1dbfc0a0fc93117869b0971cdc9b90fe4714eff
yaoleksa <mryaremchyk@gmail.com>
2026-02-03 15:09:05 +0200

    Injected UserRepository

--- src/users.service.ts
+++ src/users.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "../ecomerce";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UsersService {
 
     private readonly users: User[] = [];
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
 
     create(user: User): string {
         this.users.push(user);
