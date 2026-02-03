468bceae4be14beab6c45eb05bbda7ce3184e0e3
yaoleksa <mryaremchyk@gmail.com>
2026-02-03 11:20:02 +0200

    I have fixed DB issue!!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MeController } from './me.controller';
@@ -6,9 +7,20 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: process.env.NODE_ENV ? `src/config/.env.${process.env.NODE_ENV}` : 'src/config/.env'
-  }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV ? `src/config/.env.${process.env.NODE_ENV}` : 'src/config/.env'
+    }),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db.hmhzqphmgzyedifcbcrz.supabase.co',
+      port: 5432,
+      username: 'postgres',
+      password: 'Default_password#1',
+      database: 'postgres'
+    })
+  ],
   controllers: [AppController, MeController],
   providers: [AppService],
 })
