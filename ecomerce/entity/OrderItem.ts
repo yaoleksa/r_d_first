f3aea0ac5164302dcf240220176be16786f1ea80
yaoleksa <mryaremchyk@gmail.com>
2026-02-03 14:45:36 +0200

    fixed new DB error

--- ecomerce/entity/OrderItem.ts
+++ ecomerce/entity/OrderItem.ts
@@ -1,15 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, JoinColumn } from "typeorm";
 import { Product } from "./Product";
-import { User } from "../../src/entity/User";
+import { User } from "./User";
 
 @Entity()
 export class OrderItem {
     @PrimaryGeneratedColumn()
     id: number
     @ManyToOne(type => Product)
-    productId: number
-    // @ManyToOne(type => User)
-    // userId: number
+    @JoinColumn({
+        name: 'productId'
+    })
+    product: Product
+    @ManyToOne(type => User)
+    @JoinColumn({
+        name: 'userId'
+    })
+    owner: User
     @Column()
     price: number 
 }
