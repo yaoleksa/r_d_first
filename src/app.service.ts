f61b719eb2ce1a2f21d3f219f2873988bbec5f7a
yaoleksa <mryaremchyk@gmail.com>
2026-02-04 11:59:09 +0200

    Have improved welcome page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "This is our internet shop. There are three pages: - /users; -/products; -/orders";
   }
   helloMe(): string {
     return 'Hello Oleksa!';
