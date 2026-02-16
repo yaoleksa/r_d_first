a8c645bb1be173555705730c92d8d7d7f5e46915
yaoleksa <mryaremchyk@gmail.com>
2026-02-16 12:24:39 +0200

    running my migration

---
+++ src/data-source.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Order } from "../ecomerce/entity/Order";
+import { OrderItem } from "../ecomerce/entity/OrderItem";
+import { Product } from "../ecomerce/entity/Product";
+import { User } from '../ecomerce/entity/User';
+import dotenv from 'dotenv';
+import path from 'path';
+// Config paath to environmental variables
+dotenv.config({
+    path: path.resolve(__dirname, 'config/.env')
+});
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Order, OrderItem, Product, User],
+    migrations: ["src/migrations/*.ts"],
+    synchronize: false
+});
--- src/migrations/1771232533785-AddOrdersHotIndex.ts
+++ src/migrations/1771232533785-AddOrdersHotIndex.ts
@@ -5,7 +5,7 @@ export class AddOrdersHotIndex1771232533785 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             CREATE INDEX idx_orders_user_created_at_desc
-            ON orders (user_id, created_at DESC);
+            ON "order" ("userId", "createdAt" DESC);
             `);
     }
 
